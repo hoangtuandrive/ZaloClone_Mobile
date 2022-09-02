d5aef84b3916e87de416d4425607d0d313c770d3
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-09-02 10:55:16 +0700

    Add static data to home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 
-import { Text, Image, StyleSheet, View } from "react-native";
+import { Text, Image, StyleSheet, View, FlatList } from "react-native";
 import ChatRoomItem from "../components/ChatRoomItem/ChatRoomItem";
+
+import chatRoomsData from "../assets/dummy-data/ChatRooms";
 
 export default function HomeScreen() {
   return (
     <View style={styles.page}>
-      <ChatRoomItem />
-      <ChatRoomItem />
-      <ChatRoomItem />
+      <FlatList
+        data={chatRoomsData}
+        renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
