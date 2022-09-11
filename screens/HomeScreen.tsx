bc3efb4e8e1179b1454a99063679fdfff99d2289
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-09-11 16:54:39 +0700

    login screen, navigate

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
-
+import { useRoute, useNavigation } from "@react-navigation/native";
 import { Text, Image, StyleSheet, View, FlatList } from "react-native";
 import ChatRoomItem from "../components/ChatRoomItem/ChatRoomItem";
 import SearchBar from "../components/SearchBar/SearchBar";
 import chatRoomsData from "../assets/dummy-data/ChatRooms";
 
 export default function HomeScreen() {
+  const route = useRoute();
+  const navigation = useNavigation();
+
   return (
     <View style={styles.page}>
       <SearchBar></SearchBar>
